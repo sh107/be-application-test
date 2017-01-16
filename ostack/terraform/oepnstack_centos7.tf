b00bd9581c7e5560c1405bfbe624f9030cfc7653
Sijin He <sijin@ebi.ac.uk>
2017-01-16 10:05:53 +0000

    Update oepnstack_centos7.tf

--- ostack/terraform/oepnstack_centos7.tf
+++ ostack/terraform/oepnstack_centos7.tf
@@ -16,13 +16,13 @@ provider "openstack" {
 
 # Deploy a vanila CentoOS 7 image
 resource "openstack_compute_instance_v2" "centos7" {
-    name = "tomcat8-cOS7"
-    image_id = "8f74dd15-46ae-4ce2-b33a-75459ff8869a"
+    name = "CentOS 6.6"
+    image_id = "cd6bab98-afa4-45d1-a6ff-96f63e87e0c6"
     flavor_id = "20fce698-97de-49f1-82d0-179f3f56cf51"
-    network = {uuid="1910d716-5960-4507-b2cc-edcf73c014b7"}
+    network = {uuid="86b3ccc0-91d3-4496-9896-266747c1a870"}
 #    floating_ip = "${openstack_compute_floatingip_v2.floatip_1.address}"
-    key_pair = "jdianes_cloud_key"
-    security_groups = ["SSH-ICMP","all-open"]
+    key_pair = "phenomenal-dev2"
+    security_groups = []
 #    provisioner "local-exec" {
 #        command = "echo ${openstack_compute_instance_v2.centos7.access_ip_v4} > my_ip.txt"
 #    }
