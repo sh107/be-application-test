fbc37950188d5b0500cdd1b701d24ada6fe669b9
Sijin He <sijin@ebi.ac.uk>
2017-01-16 12:46:50 +0000

    Update oepnstack_centos7.tf

--- ostack/terraform/oepnstack_centos7.tf
+++ ostack/terraform/oepnstack_centos7.tf
@@ -18,7 +18,7 @@ provider "openstack" {
 resource "openstack_compute_instance_v2" "centos7" {
     name = "CentOS 6.6"
     image_id = "cd6bab98-afa4-45d1-a6ff-96f63e87e0c6"
-    flavor_id = "20fce698-97de-49f1-82d0-179f3f56cf51"
+    flavor_id = "87cb9ac1-2ccb-4eaf-9436-5e4d17499429"
     network = {uuid="86b3ccc0-91d3-4496-9896-266747c1a870"}
 #    floating_ip = "${openstack_compute_floatingip_v2.floatip_1.address}"
     key_pair = "phenomenal-dev2"
